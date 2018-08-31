ce20a2bff8f249ac0812b11b823d97cfc140484c
ckb <ckingbailey@gmail.com>
2018-08-31 15:21:12 -0700

    begin AssetDef model

--- model/AssetDef.php
+++ model/AssetDef.php
@@ -1,4 +1,24 @@
 <?php
 class AssetDef {
+    private $data = [
+        'assetDefID' => null,
+        'assetID' => null,
+        'defID' => null
+    ];
     
+    public function __construct($data) {
+        foreach ($this->data as $field => $val) {
+            if (empty($data[$field])) continue;
+            else $this->data[$field] = $val;
+        }
+    }
+    
+    public function insert() {
+    }
+    
+    public function update() {
+    }
+    
+    public function delete() {
+    }
 }
